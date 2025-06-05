2382d5f9dca624734c106c29336c9fa32e8d4d3b
ardaaltinors <ardaaltinors@gmail.com>
2025-06-05 18:44:26 +0300

    Enhance database configuration management

--- src/db/database.py
+++ src/db/database.py
@@ -1,18 +1,8 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
-from src.exceptions import ConfigurationError
-
-DATABASE_URL = os.getenv("DB_CONNECTION_URL")
-
-if not DATABASE_URL:
-    raise ConfigurationError(
-        message="Database connection URL is required",
-        config_key="DB_CONNECTION_URL",
-        expected_type="string"
-    )
+from src.core.config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
